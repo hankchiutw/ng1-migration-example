97b62da70aa7b6fc7a5957c52ecf22749ce83282
hank <hankchiu.tw@gmail.com>
2025-01-20 17:22:32 +0800

    refactor(ng1-app): use directive

--- src/ng1-app/ng1-app.js
+++ src/ng1-app/ng1-app.js
@@ -1,9 +1,18 @@
 import angular from 'angular';
+import template from './ng1-app.html';
 
-angular.module('myApp', []);
-angular.module('myApp').controller('MainController', [
-  '$scope',
-  function ($scope) {
+class MainController {
+  static $inject = ['$scope'];
+
+  constructor($scope) {
     $scope.title = 'Here is an AngularJS App';
-  },
-]);
+  }
+}
+
+const ng1Module = angular.module('myApp', []);
+angular.module('myApp').directive('myAppRoot', () => ({
+  controller: MainController,
+  template,
+}));
+
+export { ng1Module };
